5d16e10a7c624958c47b8cb50f2bad9ee1950b5d
jorge de la torre <jorge.delatorrecortes@gmail.com>
2023-01-18 20:11:48 -0300

    agrego division,multiplicacion y resta

--- app.js
+++ app.js
@@ -3,6 +3,41 @@ function sumar(){
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
     let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    
+    if (isNaN(resultado))
+    resultado = 'la operacion no incluye numeros'
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`resultado: ${resultado}`);
+}
+
+function restar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];    
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if (isNaN(resultado))
+    resultado = 'la operacion no incluye numeros'
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`resultado: ${resultado}`);
+}
+
+function dividir(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if (isNaN(resultado))
+    resultado = 'la operacion no incluye numeros'
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`resultado: ${resultado}`);
+}
+
+function multiplicar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    
     if (isNaN(resultado))
     resultado = 'la operacion no incluye numeros'
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
